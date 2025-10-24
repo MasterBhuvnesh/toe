3b75e69d612b7c94503289ca86163e770231ccf1
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2025-10-24 18:22:21 +0530

    1

---
+++ electron/main.ts
@@ -0,0 +1,51 @@
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+process.env.APP_ROOT = path.join(__dirname, "..");
+export const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
+export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
+
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
+  ? path.join(process.env.APP_ROOT, "public")
+  : RENDERER_DIST;
+
+let win: BrowserWindow | null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, "gdgico.svg"),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.mjs"),
+    },
+    // hide the menu bar (auto shows when Alt is pressed on Windows/Linux)
+    autoHideMenuBar: true,
+  });
+
+  // ensure menu is hidden
+  win.setMenuBarVisibility(false);
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
+}
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.whenReady().then(createWindow);
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontWeight: "bold",
+      }}
+    >
+      Version 1.0.0
+    </div>
+  );
+}
+
+export default App;
